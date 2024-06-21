d973524644dc3de8266a7774223b3f07d3d108dd
dansurlaru96 <151963327+dansurlaru96@users.noreply.github.com>
2024-06-21 10:33:51 +0300

    first init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MyHello from "./components/MyHello.vue";
+import IncDecApp from "./components/IncDecApp.vue";
+</script>
+
+<template>
+  <div>
+    <MyHello />
+    <IncDecApp />
+  </div>
+</template>
+
+<style>
+* {
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
